ca94c957a5fb41f8d8745f535addff0b8e3aa649
Jyothi vishnu vardhan Kolla <jyothivishnuvardhankolla@Jyothis-MacBook-Pro.local>
2023-04-05 13:29:33 -0400

    completed extension-4

--- examine.py
+++ examine.py
@@ -14,6 +14,7 @@ import torch
 import matplotlib.pyplot as plt
 import cv2
 import numpy
+from torchvision import models
 
 
 def get_layer1_weights(model: torch.nn.Module):
@@ -55,7 +56,7 @@ def plot_filters(filter_weights: torch.Tensor):
 
 def plot_filtered_images(conv1_weights: torch.Tensor, image: numpy.ndarray):
     """
-    akes a torch.Tensor with weights of all the
+    takes a torch.Tensor with weights of all the
     filters and displays the first image with all
     filters applied.
 
@@ -75,24 +76,114 @@ def plot_filtered_images(conv1_weights: torch.Tensor, image: numpy.ndarray):
     plt.show()
 
 
+def plot_resnet_filters(conv1_weights: torch.Tensor):
+    """
+    takes of the weights of first conv layer
+    in resnet18 and displays the first 12 filters
+    in it.
+
+    Args:
+        conv1_weights: contains the weights of first conv layer.
+    """
+    fig = plt.figure(figsize=(10, 10))
+    rows, cols = 5, 5
+    for i in range(len(conv1_weights)):
+        if i == 25:
+            break
+
+        # This is because matplotlib takes (height, width, channels)
+        img = conv1_weights[i].permute(1, 2, 0)
+        fig.add_subplot(rows, cols, i + 1)
+        plt.imshow(img)
+        plt.title(f'Figure {i}')
+        plt.axis('off')
+
+    plt.show()
+
+
+def plot_grey_scaled_resnet_filters(conv1_weights):
+    """
+    takes of the weights of first conv layer
+    in resnet18 and displays the first 12 filters
+    in it by gray scaling them.
+
+    Args:
+        conv1_weights: contains the weights of first conv layer.
+    """
+    fig = plt.figure(figsize=(10, 10))
+    rows, cols = 5, 5
+    for i in range(len(conv1_weights)):
+        if i == 25:
+            break
+        img = conv1_weights[i]
+        # greyscaling filter.
+        grey_scale_img = torch.argmax(img, dim=0, keepdim=True)
+        fig.add_subplot(rows, cols, i + 1)
+        plt.imshow(grey_scale_img.squeeze())
+        plt.title(f"Figure {i}")
+        plt.axis('off')
+
+    plt.show()
+
+
+def plot_resnet_filtered_images(conv1_weights, image):
+    """
+    takes a torch.Tensor with weights of all the
+    filters and displays the first image with all
+    filters applied.
+
+    Args:
+        conv1_weights: contains the weights of all filters in
+                       convolutional layer.
+        image: contains the image as numpy.ndarray
+    """
+    fig = plt.figure(figsize=[10, 10])
+    rows, cols = 5, 5
+    for i in range(len(conv1_weights)):
+        if i == 25:
+            break
+        filter = conv1_weights[i]
+        grey_scale_filter = torch.mean(filter, dim=0, keepdim=True).numpy()
+        filter_img = cv2.filter2D(image[0], -1, grey_scale_filter[0])
+        fig.add_subplot(rows, cols, i + 1)
+        plt.imshow(filter_img, cmap='gray')
+
+    plt.show()
+
+
 def main(argv):
-    # Load the saved model.
-    model_path = "Models/base_model.pth"
-    state_dict = torch.load(model_path, map_location=torch.device('mps'))
-    model = LeNet()
-    model.load_state_dict(state_dict)
+    examine_custom_model = int(argv[1])
+    examine_resnet_model = int(argv[2])
 
-    # Get the weights of layer-1.
-    conv1_weights = get_layer1_weights(model)
-
-    # Plot weights of all filters in layer-1.
-    plot_filters(conv1_weights)
-
-    # Plot the image after applying filters.
     train_data, test_data, class_names = create_dataloaders(32)
     image, label = next(iter(train_data))
     image = image[0].numpy()
-    plot_filtered_images(conv1_weights, image)
+
+    if examine_custom_model == 1:  # If true examine the custom model.
+        # Load the saved model.
+        model_path = "Models/base_model.pth"
+        state_dict = torch.load(model_path, map_location=torch.device('mps'))
+        model = LeNet()
+        model.load_state_dict(state_dict)
+
+        # Get the weights of layer-1.
+        conv1_weights = get_layer1_weights(model)
+
+        # Plot weights of all filters in layer-1.
+        plot_filters(conv1_weights)
+
+        # Plot the image after applying filters.
+        plot_filtered_images(conv1_weights, image)
+
+    if examine_resnet_model == 1:  # if true examine resnet model.
+        model_resnet = models.resnet18(pretrained=True)
+        # get weights of first conv layer.
+        conv1_weights = model_resnet.state_dict()['conv1.weight']
+
+        plot_resnet_filters(conv1_weights)
+        plot_grey_scaled_resnet_filters(conv1_weights)
+
+        plot_resnet_filtered_images(conv1_weights, image)
 
 
 if __name__ == "__main__":
